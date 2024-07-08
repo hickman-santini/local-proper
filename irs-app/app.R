1147e6b94d19cb4f8c6347de71e210b0e04d12f8
Molly Hickman <mhickman@mitre.org>
2024-07-07 23:11:43 -0400

    manifold reliability, better explanation

--- irs-app/app.R
+++ irs-app/app.R
@@ -3,12 +3,20 @@ library(ggplot2)
 
 ui <- fluidPage(
   titlePanel("Instant Reliability Significance"),
+  h3(HTML("<b>How should we interpret reliability graphs?</b>")),
+  p("What's important is consistency, not distance from the 'perfect calibration' line."),
+  div(
+    img(src = "silver-BRAGGING-01-1.png", height = "300px"),
+    p(HTML("Source: <a href='https://fivethirtyeight.com/features/when-we-say-70-percent-it-really-means-70-percent/' target='_blank'>FiveThirtyEight</a>"), style = "text-align: left; font-style: italic;")
+  ),
+  p("This handy simulator tells you what the chances are that [N] forecasts of [p_gov] would be 'aligned' in only [threshold] or fewer of the outcomes, e.g. in this example from FiveThirtyEight, if the true probability of a home-team win were 85% and there were 383 games, what are the chances that only 79% of those games would be home-wins? Actually very small, suggesting we shouldn't be that impressed."),
+  p(HTML("For more information, refer to Bröcker, J. and Smith, L.A. (2007) <a href='https://journals.ametsoc.org/view/journals/wefo/22/3/waf993_1.xml?tab_body=pdf'>'Increasing the reliability of reliability diagrams'</a>, Weather and Forecasting, 22(3): 651-661.")),
   sidebarLayout(
     sidebarPanel(
-      numericInput("p_gov", "Govering probability (p_gov):", value = 0.85),
       numericInput("N", "Number of p_gov forecasts:", value = 383),
+      numericInput("p_gov", "Governing probability (p_gov):", value = 0.85),
       numericInput("lessthan", "Threshold of interest:", value = 0.79),
-      numericInput("mw", "Worlds:", value = 4096),
+      numericInput("mw", "Worlds:", value = 8192),
       actionButton("submit", "Submit")
     ),
     mainPanel(
